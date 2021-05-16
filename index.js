bf646d17e949b988ecc86066dda3dbc98bdd2b16
Gokul-Cloud <gokulgiritv@gmail.com>
2021-05-16 16:43:28 +0530

    test 2

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.use('/posts', postRoutes);
 const PORT = process.env.PORT;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
