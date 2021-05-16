890559eddc2b83b29fe98b54d617c2ae10701edd
Gokul-Cloud <gokulgiritv@gmail.com>
2021-05-16 15:51:35 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,24 @@
+
+import express from 'express';
+
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
+const app = express();
+dotenv.config();
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+
+const PORT = process.env.PORT;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
